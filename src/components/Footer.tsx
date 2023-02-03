c9c98ddb91ba937db9105a2a81bdd530ea9f2f4f
User-Wellengton <wellengtonjm@gmail.com>
2023-02-03 10:13:27 -0300

    Att

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-        <p>
-          <span>React + TS Todo</span> @2022
-        </p>
+      <p>
+        <span>React + TS Todo</span> @2023
+      </p>
     </footer>
-  )
+  );
 };
 
 export default Footer;
